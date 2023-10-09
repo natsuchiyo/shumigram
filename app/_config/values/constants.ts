e39793d4f01074d29ad520723ee254934c5f1061
natsuchiyo <natsuchiyo77@gmail.com>
2023-10-10 04:55:30 +0900

    change image

--- app/_config/values/constants.ts
+++ app/_config/values/constants.ts
@@ -1,7 +1,4 @@
 export const isProduction = process.env.NODE_ENV === "production";
 export const isDevelop = process.env.NODE_ENV === "development";
 export const isTest = process.env.NODE_ENV === "test";
-export const baseUrl = process.env.GTHB_SERVER ? "/shumigram" : "";
-
-console.log('↓-------------------------------------------------------------------------');
-console.log(process.env);
+export const baseUrl = process.env.GITHUB_ACTIONS ? "/shumigram" : "";
--- next.config.js
+++ next.config.js
@@ -10,8 +10,8 @@ const nextConfig = {
   // swcMinify: false,
   distDir: distDir,
   // productionBrowserSourceMaps: true,
-  basePath: process.env.GTHB_SERVER ? "/shumigram" : "",
-  // assetPrefix: process.env.GTHB_SERVER ? "/shumigram" : "",
+  basePath: process.env.GITHUB_ACTIONS ? "/shumigram" : "",
+  // assetPrefix: process.env.GITHUB_ACTIONS ? "/shumigram" : "",
   images: {
     loader: "custom",
     // unoptimized: true,
