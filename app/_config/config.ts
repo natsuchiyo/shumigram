de664f70088f58a29ecaf726b268d5e73d913d02
natsuchiyo <40353051+natsuchiyo@users.noreply.github.com>
2023-12-17 00:55:06 +0900

    update

--- app/_config/config.ts
+++ app/_config/config.ts
@@ -2,6 +2,10 @@ import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 import { createConfig } from "../_common/functions/configs/createConfig";
 
 
+console.log('↓-------------------------------------------------------------------------');
+console.log(process.env);
+console.log(process.env.GITHUB_ACTIONS ? "/shumigram" : "");
+
 export default createConfig({
     websiteLabelName: "シュミグラム",
     websiteName: "shumigram",
