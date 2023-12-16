f8033a942d61ec281a6721020467da0eed0bc344
natsuchiyo <40353051+natsuchiyo@users.noreply.github.com>
2023-12-16 22:36:19 +0900

    create peopattern-gen

--- app/_components/layouts/_values.ts
+++
@@ -1,8 +0,0 @@
-
-export const headerHeight = '40px';
-
-export const leftNavWidth = '250px';
-
-export const asideWidth = '300px';
-
-export const footerHeight = 'auto';
--- app/_components/pieces/DebugComponent.tsx
+++
@@ -1,8 +0,0 @@
-import { notice } from "../../_functions/console";
-
-export const DebugComponent = (props: { text?: string; }) => {
-
-    notice('DebugComponent--------------' + props.text + '------------------------');
-
-    return null;
-};
