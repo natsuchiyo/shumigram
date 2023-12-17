050e44f9238fb12d5291e255ea15e1ddef5c786e
natsuchiyo <40353051+natsuchiyo@users.noreply.github.com>
2023-12-18 03:16:00 +0900

    update

--- app/_config/infomationList.tsx
+++ app/_config/infomationList.tsx
@@ -5,6 +5,7 @@ import { getPageLink } from "../_common/functions/urls/getPageLink";
 
 
 export default createInfomationList([
+    { date: '2023/12/16', text: <>{getPageLink('/geopattern-gen')}を作成しました</> },
     { date: '2023/10/11', text: <>{getPageLink('/block-pazzle')}を作成しました</> },
     { date: '2023/10/9', text: <>{getPageLink('/janken-wars')}を作成しました</> },
 ]);
